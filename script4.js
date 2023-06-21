9f7737b61d495caae60cfb9e1975846c6c8bc0e6
harishspatil <73633770+harishspatil@users.noreply.github.com>
2023-06-21 15:23:28 +0530

    deleteButton

--- script4.js
+++ script4.js
@@ -16,9 +16,20 @@ let myObj_serialized = JSON.stringify(myObj);
 localStorage.setItem('myObj',myObj_serialized);
 
 var details = "Name: " + firstname + " Email: " + email + " Phone Number" + number;
-detailsDiv.innerHTML = details;
+// detailsDiv.innerHTML = details;
+
+var deleteButton = document.createElement("button");
+deleteButton.textContent = "Delete";
+deleteButton.addEventListener("click", function() {
+  // Remove the item from local storage
+  localStorage.removeItem('myObj');
+  // Clear the detailsDiv
+  detailsDiv.innerHTML = "";
 
 
+});
 
-
+detailsDiv.innerHTML = "";
+detailsDiv.appendChild(deleteButton);
+detailsDiv.insertAdjacentHTML("beforeend", details);
 })
