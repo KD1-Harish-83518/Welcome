4dbdd14393871722861662cf4a9fc21824602a82
harishspatil <73633770+harishspatil@users.noreply.github.com>
2023-06-21 12:51:27 +0530

    Objecgt

--- script4.js
+++ script4.js
@@ -1,7 +1,15 @@
 document.getElementById('detailsForm').addEventListener('submit', function(){
-var firstname = document.getElementById('name').value;
-var lastname = document.getElementById('lname').value;
+    var firstname = document.getElementById('name').value;
+    var lastname = document.getElementById('lname').value;
+    let myObj = {
+        name: `${firstname}`,
+        lname: `${lastname}`
 
-localStorage.setItem('name',firstname);
-localStorage.setItem('lname',lastname);
+    }
+    
+let myObj_serialized = JSON.stringify(myObj);
+
+
+localStorage.setItem('myObj',myObj_serialized);
+
 })
