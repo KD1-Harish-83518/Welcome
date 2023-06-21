ca16099cadfa711eb6b017f022e47e9a57cdf403
harishspatil <73633770+harishspatil@users.noreply.github.com>
2023-06-21 11:24:15 +0530

    filtercommit

--- script3.js
+++ script3.js
@@ -48,3 +48,23 @@ function removeItem(e){
     }
   }
 }
+
+// filterItems
+
+function filterItems(e){
+    //convert text to lower case
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text)!=-1){
+            item.style.display = 'block';
+
+
+        }
+        else{
+            item.style.display="none"
+        }
+    })
+
+}
