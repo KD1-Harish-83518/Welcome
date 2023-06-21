5527507efaaa34b78a3d100018c60327b325e6d3
harishspatil <73633770+harishspatil@users.noreply.github.com>
2023-06-21 14:18:48 +0530

    DisplayDetails

--- script4.js
+++ script4.js
@@ -1,15 +1,24 @@
-document.getElementById('detailsForm').addEventListener('submit', function(){
+document.getElementById('detailsForm').addEventListener('submit', function(e){
+e.preventDefault();
     var firstname = document.getElementById('name').value;
-    var lastname = document.getElementById('lname').value;
+    var email = document.getElementById('email').value;
+    var number = document.getElementById('lname').value;
+    var detailsDiv = document.getElementById("details");
+   
     let myObj = {
         name: `${firstname}`,
-        lname: `${lastname}`
+        email: `${email}`,
+        number: `${number}`
 
     }
     
 let myObj_serialized = JSON.stringify(myObj);
-
-
 localStorage.setItem('myObj',myObj_serialized);
+
+var details = "Name: " + firstname + " Email: " + email + " Phone Number" + number;
+detailsDiv.innerHTML = details;
+
+
+
 
 })
