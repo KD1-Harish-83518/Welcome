2279fdcef4b4052feed96c519e4dd2c5b160f600
harishspatil <73633770+harishspatil@users.noreply.github.com>
2023-06-26 12:34:02 +0530

    asda

--- script6.js
+++ script6.js
@@ -1,3 +1,44 @@
+// Function to edit user details
+function editUser(userId, userName) {
+    const editForm = document.getElementById('editForm');
+    const editUserIdInput = document.getElementById('editUserId');
+    const editNameInput = document.getElementById('editName');
+
+    editUserIdInput.value = userId;
+    editNameInput.value = userName;
+
+    editForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const updatedUserName = editNameInput.value;
+
+        fetch('/api/users/' + userId, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name: updatedUserName })
+        })
+        .then(response => {
+            if (response.ok) {
+                // Update the user detail on the website
+                const userElement = document.getElementById('user-' + userId);
+                const userDetails = userElement.querySelector('.user-details');
+                userDetails.textContent = updatedUserName;
+
+                // Reset the edit form
+                editUserIdInput.value = '';
+                editNameInput.value = '';
+            } else {
+                console.error('Failed to edit user detail');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+}
+
 // Function to delete a user detail
 function deleteUser(userId) {
     fetch('/api/users/' + userId, {
@@ -28,6 +69,13 @@ function displayUserDetails(user) {
     userDetails.classList.add('user-details');
     userDetails.textContent = user.name;
 
+    const editIcon = document.createElement('span');
+    editIcon.classList.add('edit-icon');
+    editIcon.textContent = '✏️';
+    editIcon.addEventListener('click', () => {
+        editUser(user._id, user.name);
+    });
+
     const deleteIcon = document.createElement('span');
     deleteIcon.classList.add('delete-icon');
     deleteIcon.textContent = '❌';
@@ -36,6 +84,7 @@ function displayUserDetails(user) {
     });
 
     userElement.appendChild(userDetails);
+    userElement.appendChild(editIcon);
     userElement.appendChild(deleteIcon);
     userList.appendChild(userElement);
 }
